3bfac11b03ea6c16df7aa0e271936a3088190962
flitterio <francesca@litterio.net>
2020-12-16 18:23:56 -0500

    need to fix bug for login

--- src/SignIn/SignIn.js
+++ src/SignIn/SignIn.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import TokenService from '../services/token-service';
+import AuthApiService from '../services/auth-api-service';
 import {Button, Input } from '../Utils/Utils';
 
 class SignIn extends Component {
@@ -22,13 +23,33 @@ class SignIn extends Component {
         this.props.onSigninSuccess()
     }
 
+    handleSubmitJwtAuth = ev => {
+           ev.preventDefault()
+           this.setState({ error: null })
+           const { username, password } = ev.target
+        
+           AuthApiService.postSignin({
+             username: username.value,
+             password: password.value,
+           })
+             .then(res => {
+               username.value = ''
+               password.value = ''
+               TokenService.saveAuthToken(res.authToken)
+               this.props.onSigninSuccess()
+             })
+             .catch(res => {
+               this.setState({ error: res.error })
+             })
+         }
+
     render(){
         const {error } = this.state
         return(
             <div className="signIn">
                 <h1> Login </h1>
             <form id="login"
-                onSubmit={this.handleSubmitBasicAuth}>
+               onSubmit={this.handleSubmitJwtAuth}>
                     <div role='alert'>
                         {error && <p className='red'>{error}</p>}
                     </div>
