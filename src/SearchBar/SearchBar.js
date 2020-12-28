8c7354b1d7412bbecb31c8f0768f29699ce4c404
flitterio <francesca@litterio.net>
2020-12-27 19:29:17 -0500

    fixed search bar mostly, just missing favorite search attribute

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -2,82 +2,152 @@ import React from 'react';
 import Item from '../Item/Item';
 import ApiContext from '../ApiContext';
 import Select from 'react-select';
-import{ groupedOptions} from '../options'
+import{ seasons} from '../options'
 
 class SearchBar extends React.Component{
+    static contextType = ApiContext;
     state={
         search: '',
         season: '',
         category: '',
+        favorite: false,
+        
     };
-    static contextType = ApiContext;
 
+    clearSearch =(event) => {
+        window.location.href='/my-closet'
+    }
     handleWordSearch= (event) =>{
         this.setState({search: event.target.value})
     }
 
-    handleSeasonChange = (event) => {
-        this.setState({season: event.target.value})
+    handleSeasonChange = (e) => {
+        let newSeasons = Array.isArray(e) ? e.map(x=> x.value) : []
+        this.setState({
+            season: newSeasons.join(', ')
+        })
     }
     handleCategoryFilter = (event) => {
-        this.setState({ category: Array.isArray(event) ? event.map(x=> x.value) : []})
+        this.setState({category: event.target.value})
     }
+    handleFavoriteFilter = (event) => {
+        const current = this.state.favorite;
+        const newVal = !current;
+         this.setState({ favorite: newVal }); 
+    };
+
+    // handleSearch = (event) => {
+    //     event.preventDefault();
+    //     this.setState({searchItems: this.context.items})
+    //     const {searchItems} = this.state
+ 
+    //     {let wordSearch= searchItems.filter(
+    //         (item) => {
+    //             return item.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+    //         }
+    //     ) 
+    //         this.setState({
+    //             searchItems: wordSearch
+    //         })}
+
+    //     if(this.state.season !== '')
+    //     {let seasonFilter = searchItems.filter(
+    //         (item) =>{
+    //             return item.season.toLowerCase().indexOf(this.state.season.toLowerCase()) !== -1;
+    //         }
+    //     )
+    //         this.setState({
+    //             searchItems: seasonFilter
+    //         })}
+
+    //     if(this.state.category !== '')
+    //     {let categoryFilter = searchItems.filter(
+    //         (item) =>{
+    //             return item.season.toLowerCase().indexOf(this.state.season.toLowerCase()) !== -1;
+    //         }
+    //     )
+    //             this.setState({
+    //                 searchItems: categoryFilter
+    //             })}
+
+    //     if(this.state.favorite === true){
+    //         let favoriteFilter = searchItems.filter(
+    //             (item) =>{
+    //                return item.favorite === true
+    //             }
+    //         )
+                    
+    //         this.setState({
+    //             searchItems: favoriteFilter
+    //         })
+    //             } 
+    //     }
+
+
 render(){
     let {items=[]} = this.context;
+
     let searchItems= items.filter(
         (item) => {
-            return item.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+            return item.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1 &&
+            item.season.toLowerCase().indexOf(this.state.season.toLowerCase()) !== -1 &&
+            item.category.toLowerCase().indexOf(this.state.category.toLowerCase()) !== -1
+            
         }
     ) 
-    let seasonFilter = searchItems.filter(
-        (item) =>{
-            return item.season.toLowerCase().indexOf(this.state.season.toLowerCase()) !== -1;
-        }
-
-    )
-    let categoryFilter = seasonFilter.filter(
-        (item) =>{
-            return item.season.toLowerCase().indexOf(this.state.season.toLowerCase()) !== -1;
-        }
-
-    )
 
     return(
-        <div className="searchbar">
+    <div className="searchbar">
         <form className='searchbar'
-            onSubmit={this.handleSearch}>
+        onSubmit={this.handleSearch}>
             <input 
-            type="text" 
-            placeholder="Search"
-            value={this.state.search}
-            onChange={this.handleWordSearch}/>
+                type="text" 
+                placeholder="Search"
+                value={this.state.search}
+                onChange={this.handleWordSearch}/>
 
-     <select name="season" id="season" form="season" onChange={this.handleSeasonChange}>
-         <option value='' >Season...</option>
-         <option value="winter">Winter</option>
-         <option value="spring">Spring</option>
-         <option value="summer">Summer</option>
-         <option value="fall">Fall</option>
+                    <Select  
+                        id="season"
+                        options={seasons}
+                        onChange={this.handleSeasonChange}
+                        isMulti
+                        isClearable/>
 
-         </select>
-         <Select id="category"
-                    options={groupedOptions}
-                    onChange={this.handleCategoryFilter}
-                    isClearable
-                />
-        </form>
+            <select value={this.state.category}
+                onChange={this.handleCategoryFilter}
+                name="category" id="category" form="category">
+                    <option value='' >Category...</option>
+                    <option value="Top">Tops</option>
+                    <option value="Bottom">Bottoms</option>
+                    <option value="Dress">Dress/Romper/Pantsuit/Jumpsuit</option>
+                    <option value="Outerwear">Outerwear</option>
+                    <option value="Shoes">Shoes</option>
+                    <option value="Accessories">Accessories</option>
+                    <option value="Sleepwear">Sleepwear</option>
+                    <option value="Undergarments">Undergarments</option>
+                    <option value="Other">Other</option>
 
-        <div className='group'>
-                {categoryFilter.map(item =>
-                    <div className='item' key={item.id}>
-                        <Item 
-                            id={item.id}
-                            image={item.image}
-                            title={item.title}
-                                />
-                            </div>
-                         )}
+                </select>
+                <input 
+                    name="favorite" 
+                    type="checkbox"
+                    checked={this.state.favorite}  
+                    onChange={this.handleFavoriteFilter} /> Favorite?
+
+                <button type='button' name='clear' onClick={this.clearSearch}>Clear Search</button>
+            </form>
+            <div className='group'>
+                    {searchItems.map(item =>
+                        <div className='item' key={item.id}>
+                            <Item 
+                                id={item.id}
+                                image={item.image}
+                                title={item.title}
+                                    />
+                                </div>
+                            )}
                 </div>
+
         </div>
     )}
 }
